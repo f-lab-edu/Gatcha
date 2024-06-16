43272e36f777e4bfa2b5f16d9ca6e3f9a45a81aa
vamlin <lsc991123@gmail.com>
2024-06-16 19:55:51 +0900

    init

---
+++ src/test/java/org/example/gacha/GachaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gacha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GachaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
